14fb3867a5d461995c1cd5490490012daa05b62d
Peal Mazumder <peal.mazumder@brainstation-23.com>
2025-02-03 14:04:33 +0600

    Revert "refactor: ensure network call is main-safe by using Dispatchers.IO"
    This reverts commit 7b3f53b22c2e27e78f9d4a18c0dea305ff573fda.

--- app/src/main/java/com/peal/spacestationapp/core/data/network/safeCall.kt
+++ app/src/main/java/com/peal/spacestationapp/core/data/network/safeCall.kt
@@ -2,26 +2,23 @@ package com.peal.spacestationapp.core.data.network
 
 import com.peal.spacestationapp.core.domain.util.NetworkError
 import com.peal.spacestationapp.core.domain.util.Result
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.withContext
 import retrofit2.Response
 import java.io.IOException
+import kotlin.coroutines.coroutineContext
 
 suspend inline fun <reified T> safeCall(
-    crossinline apiCall: suspend () -> Response<T>
+    apiCall: () -> Response<T>
 ): Result<T, NetworkError> {
-    return withContext(Dispatchers.IO) {
-        val response = try {
-            apiCall()
-        } catch (e: IOException) {
-            return@withContext Result.Failure(NetworkError.NO_INTERNET)
-        } catch (e: Exception) {
-            coroutineContext.ensureActive()
-            return@withContext Result.Failure(NetworkError.UNKNOWN)
-        }
-        responseToResult(response)
+    val response = try {
+        apiCall()
+    } catch (e: IOException) {
+        return Result.Failure(NetworkError.NO_INTERNET)
+    } catch (e: Exception) {
+        coroutineContext.ensureActive()
+        return Result.Failure(NetworkError.UNKNOWN)
     }
-}
 
+    return responseToResult(response)
+}
 
